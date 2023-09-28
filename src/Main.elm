167420374de5ebe9703b977412e587413ce3c12c
Maina <PMaina@midwayusa.com>
2023-09-28 16:28:33 -0500

    static enlarge selected image

--- src/Main.elm
+++ src/Main.elm
@@ -5,19 +5,47 @@ import Html exposing (Html, div, h1, img, text)
 import Html.Attributes exposing (..)
 
 
+urlPrefix : String
+urlPrefix =
+    "http://elm-in-action.com/"
+
+
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
         , div [ id "thumbnails" ]
-            [ img [ src "http://elm-in-action.com/1.jpeg" ] []
-            , img [ src "http://elm-in-action.com/2.jpeg" ] []
-            , img [ src "http://elm-in-action.com/3.jpeg" ] []
+            (List.map
+                (\photo -> viewThumbnail model.selectedUrl photo)
+                model.photos
+            )
+        , img
+            [ class "large"
+            , src (urlPrefix ++ "large/" ++ model.selectedUrl)
             ]
+            []
         ]
 
 
+viewThumbnail selectedUrl thumb =
+    img
+        [ src (urlPrefix ++ thumb.url)
+        , classList [ ( "selected", selectedUrl == thumb.url ) ]
+        ]
+        []
+
+
+initialModel =
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selectedUrl = "1.jpeg"
+    }
+
+
 main =
-    view ""
+    view initialModel
 
 
 
